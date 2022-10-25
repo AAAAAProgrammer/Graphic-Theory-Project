64ac8d15238842e45547e3f00282362371a57772
AAAAAProgrammer <oabo62674@gmail.com>
2022-10-24 21:25:36 -0700

    The First of The Project

---
+++ GraphTheory-Project/Form1.Designer.cs
@@ -0,0 +1,95 @@
+﻿namespace GraphTheory_Project
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panl1 = new System.Windows.Forms.Panel();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.chboxGrid = new System.Windows.Forms.CheckBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // panl1
+            // 
+            this.panl1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.panl1.Location = new System.Drawing.Point(730, 185);
+            this.panl1.Name = "panl1";
+            this.panl1.Size = new System.Drawing.Size(661, 504);
+            this.panl1.TabIndex = 0;
+            this.panl1.Paint += new System.Windows.Forms.PaintEventHandler(this.panl1_Paint);
+            this.panl1.MouseClick += new System.Windows.Forms.MouseEventHandler(this.panl1_MouseClick);
+            this.panl1.MouseMove += new System.Windows.Forms.MouseEventHandler(this.Form1_MouseMove);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pictureBox1.Location = new System.Drawing.Point(25, 34);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(667, 565);
+            this.pictureBox1.TabIndex = 1;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.MouseClick += new System.Windows.Forms.MouseEventHandler(this.pictureBox1_MouseClick);
+            this.pictureBox1.MouseMove += new System.Windows.Forms.MouseEventHandler(this.pictureBox1_MouseMove);
+            // 
+            // chboxGrid
+            // 
+            this.chboxGrid.AutoSize = true;
+            this.chboxGrid.Location = new System.Drawing.Point(45, 626);
+            this.chboxGrid.Name = "chboxGrid";
+            this.chboxGrid.Size = new System.Drawing.Size(75, 17);
+            this.chboxGrid.TabIndex = 2;
+            this.chboxGrid.Text = "Show Grid";
+            this.chboxGrid.UseVisualStyleBackColor = true;
+            this.chboxGrid.CheckStateChanged += new System.EventHandler(this.chboxGrid_CheckStateChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1420, 701);
+            this.Controls.Add(this.chboxGrid);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.panl1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.MouseClick += new System.Windows.Forms.MouseEventHandler(this.panl1_MouseClick);
+            this.MouseMove += new System.Windows.Forms.MouseEventHandler(this.Form1_MouseMove);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panl1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.CheckBox chboxGrid;
+    }
+}
+
---
+++ GraphTheory-Project/Form1.cs
@@ -0,0 +1,116 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace GraphTheory_Project
+{
+    public partial class Form1 : Form
+    { Point MouseCordinate;
+        Graphics g;
+        Pen P;
+        int k = 0;
+        Point[] points = new Point[50];
+
+        
+        int new_X = 0;
+        int new_Y = 0;
+        int lines = 20;
+        int W_Space;
+        int H_Space;
+        public Form1()
+        {
+            InitializeComponent();
+            g = pictureBox1.CreateGraphics();
+            P = new Pen(Color.Black, 3);
+            W_Space = pictureBox1.Width / 20;
+            H_Space = pictureBox1.Height / 20;
+        }
+        
+        private void Form1_MouseMove(object sender, MouseEventArgs e)
+        {
+            
+        }
+
+        private void panl1_MouseClick(object sender, MouseEventArgs e)
+        {
+            
+            
+
+        }
+
+        private void DrawGrid()
+        {
+            Graphics gr = pictureBox1.CreateGraphics();
+            Pen P_gr = new Pen(Brushes.Black, 1);
+            
+            float x = 0f;
+            float y = 0f;
+            float xSpace = pictureBox1.Width / lines;
+            float ySpace = pictureBox1.Height / lines;
+
+
+
+
+            
+            for (int i = 0; i < lines; i++)
+            {
+                
+                gr.DrawLine(P_gr, x, y, x, pictureBox1.Height);
+                x += xSpace;
+            }
+            x = 0f;
+            for (int i = 0; i < lines; i++)
+            {
+                
+                gr.DrawLine(P_gr, x, y, pictureBox1.Width, y);
+                y+=ySpace;
+            }
+
+
+        }
+
+        private void panl1_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void pictureBox1_MouseClick(object sender, MouseEventArgs e)
+        {
+            int new_x = MouseCordinate.X / W_Space;
+            int new_y = MouseCordinate.Y / H_Space;
+            
+            int x_center = new_x * W_Space + Convert.ToInt32(0.5 * W_Space);
+            int y_center = new_y * H_Space + Convert.ToInt32(0.5 * H_Space);
+
+            g.DrawEllipse(P, x_center-12 , y_center - 12 , 25, 25);
+            points[k++] = new Point(MouseCordinate.X, MouseCordinate.Y);
+        }
+
+        private void pictureBox1_MouseMove(object sender, MouseEventArgs e)
+        {
+            MouseCordinate = pictureBox1.PointToClient(Cursor.Position);
+            
+            Console.WriteLine(MouseCordinate);
+            
+
+        }
+
+        private void chboxGrid_CheckStateChanged(object sender, EventArgs e)
+        {
+            if (chboxGrid.Checked)
+            {
+                DrawGrid();
+            }
+            else
+            {
+                //pictureBox1.Invalidated();
+            }
+        }
+    }
+}
