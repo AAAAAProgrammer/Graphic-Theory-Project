cf0849f921310de4a9c0d97395bdadb045f0a969
AAAAAProgrammer <oabo62674@gmail.com>
2022-11-21 20:04:24 -0800

    The First In recognize my Code  : add tow clases (Node , Graph)

---
+++ GraphTheory-Project/Node.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Drawing;
+
+namespace GraphTheory_Project
+{
+    class Node
+    {
+        int X;
+        int Y;
+        System.Windows.Forms.Label L;
+        int wieght;
+        int Degree;
+        static int index = 0;
+        public Node ()
+        {
+
+        }
+
+        public int _X { get; }
+        public int _Y { get; }
+
+        public void CreatNode(int x,int y)
+        {
+            X = x;
+            Y = y;
+
+            index++;
+            L = new Label();
+            L.Location = new Point(x, y);
+            L.Name = index.ToString();
+            L.Font = new Font(FontFamily.GenericSansSerif, 9, FontStyle.Underline);
+            L.Size = new Size(13, 13);
+            L.Text = Convert.ToString(index);
+            L.TabIndex = 3;
+        }
+
+       
+    
+    }
+}
